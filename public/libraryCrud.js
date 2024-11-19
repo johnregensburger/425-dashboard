7791f5ee41c603f3d87cecc8f114e948e96079e0
johnregensburger <johnregensburger2@gmail.com>
2024-11-19 13:23:51 -0600

    Created method signatures

--- public/libraryCrud.js
+++ public/libraryCrud.js
@@ -4,3 +4,23 @@ const db = require("./database.js");
 /* TODO:
     CRUD functions for library entries
 */
+
+// Creates a library entry based on the user and what game they've wishlisted or now own
+async function createEntry(userId, gameId, status) {
+
+}
+
+// Reads a library entry based on the entry ID
+async function readEntry(id) {
+
+}
+
+// Updates an attribute of a library entry
+async function updateEntry(id, column, newValue) {
+
+}
+
+// Deletes a library entry
+async function deleteEntry(id) {
+
+}
