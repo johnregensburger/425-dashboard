93b5615b10db0ab37c154119bebb0d6c688d6bdd
kiera taylor <156949551+ghostlyspooky@users.noreply.github.com>
2024-12-03 22:59:08 -0600

    cleaning this shit UP

--- src/front.jsx
+++ src/front.jsx
@@ -1,192 +1,178 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 const Front = () => {
+  const navigate = useNavigate();
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [fromValue, setFromValue] = useState(1); //filter min
+  const [toValue, setToValue] = useState(8);     //filter max
+  const [games, setGames] = useState([]);
+  const [visibleGames, setVisibleGames] = useState(20); // Start by showing 20 games
 
-const navigate = useNavigate();
-const [isLoggedIn, setIsLoggedIn] = useState(false);
-const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-const [fromValue, setFromValue] = useState(1);
-const [toValue, setToValue] = useState(8);
-const [games, setGames] = useState([]);
-const [visibleGames, setVisibleGames] = useState(20); // Start by showing 20 games
+  const checkLoginStatus = async () => {
+    try {
+      const response = await fetch('http://localhost:3000/test-session', {
+        method: 'GET',
+        credentials: 'include', // include user id in req
+      });
 
-const checkLoginStatus = async () => {
-  try {
-    const response = await fetch('http://localhost:3000/test-session', {
-      method: 'GET',
-      credentials: 'include', // Include cookies in the request
-    });
+      if (response.ok) {
+        const data = await response.json();
+        const userId = data.userID; // Extract user ID from response
+        setIsLoggedIn(true);        //page recognizes you're logged in and adjusts
+      } else {
+        const errorData = await response.json();
+        console.error('No active session:', errorData);
+      }
+    } catch (error) {
+      console.error('Error fetching user ID:', error);
+      alert('Error fetching user ID.');
+  }};
 
-    if (response.ok) {
+  const toggleSidebar = () => { 
+    setIsSidebarOpen(prevState => !prevState); //when ☰ is clicked it adjusts the page
+  };
+
+  const logOut = () => {
+    setIsLoggedIn((prevState) => !prevState);   //tells the page you're logged out
+    navigate('/'); //nav to the Login page and destroys your session token(?)
+  };
+
+  const logIn = () => {
+    navigate('/'); //nav to the Login page
+  };
+
+  const goToLibrary = () => {
+    navigate('/library'); //nav to library
+  };
+
+  const goToInfo = (id, loc) => {  //passes in what page you came from and what game you clicked on
+    navigate(`/info/${id}/${loc}`); //go to page of game you clicked on
+  };
+
+  const fetchGames = async () => {
+    try {
+      const response = await fetch(`http://localhost:3000/games`);  //gets all games in the database
+
+      if (!response.ok) {
+        throw new Error('Failed to fetch games');
+      }
+      const data = await response.json(); 
+      setGames(data); //adds data to page
+    } catch (error) {
+      console.error('Error fetching games:', error);
+  }};
+
+  const fetchFilter = async (fromValue, toValue) => {
+    try {   //fetches games only at specified filter value only when the filter button is pressed
+      const response = await fetch(`http://localhost:3000/games/filter?minPlayers=${fromValue}&maxPlayers=${toValue}`);
+
+      if (!response.ok) {
+        throw new Error('Failed to fetch games');
+      }
       const data = await response.json();
-      const userId = data.userID; // Extract user ID from response
-      setIsLoggedIn(true);
-      console.log('User ID:', userId);
-    } else {
-      const errorData = await response.json();
-      console.error('No active session:', errorData);
-    }
-  } catch (error) {
-    console.error('Error fetching user ID:', error);
-    alert('Error fetching user ID.');
-  }
-};
+      setGames(data); //adds data to page
+    } catch (error) {
+      console.error('Error fetching games:', error);
+  }};
 
-const toggleSidebar = () => {
-  setIsSidebarOpen(prevState => !prevState);
-};
+  const loadMoreGames = () => { //loads more games into the scrollbox
+    setVisibleGames((prev) => Math.min(prev + 20, games.length));
+  };
 
- const logOut = () => {
-  setIsLoggedIn((prevState) => !prevState);
-   navigate('/'); // Navigate to the Login page
- };
+  useEffect(() => { //runs automatically when the page opens. order matters
+    fetchGames();
+    checkLoginStatus();
+  }, []);
 
- const logIn = () => {
-  navigate('/'); // Navigate to the Login page
- }
-
- const goToLibrary = () => {
-  navigate('/library');
- }
-
- const goToInfo = (id, loc) => {
-  navigate(`/info/${id}/${loc}`);
-}
-
-const fetchGames = async () => {
-  try {
-    const response = await fetch(`http://localhost:3000/games`);
-    if (!response.ok) {
-      throw new Error('Failed to fetch games');
-    }
-    console.log('games fetched');
-    const data = await response.json(); // Fetch the data
-    setGames(data); //updates the state
-  } catch (error) {
-    console.error('Error fetching games:', error);
-  }
-};
-
-const fetchFilter = async (fromValue, toValue) => {
-  try {
-    const response = await fetch(`http://localhost:3000/games/filter?minPlayers=${fromValue}&maxPlayers=${toValue}`);
-    if (!response.ok) {
-      throw new Error('Failed to fetch games');
-    }
-    console.log('games fetched');
-    const data = await response.json(); // Fetch the data
-    setGames(data); //updates the state
-  } catch (error) {
-    console.error('Error fetching games:', error);
-  }
-};
-
-const loadMoreGames = () => { //loads more games into the scrollbox
-  setVisibleGames((prev) => Math.min(prev + 20, games.length));
-};
-
-useEffect(() => {
-  fetchGames();
-  checkLoginStatus();
-}, []);
-
-   return ( 
-     <div className={`app ${isSidebarOpen ? "sidebar-open" : ""}`}>
-      {/* Header Section */}
-      <header>
-        <div className="header-left">
-          <button className="open-btn" onClick={toggleSidebar}>
-            ☰ Filter
-          </button>
-        </div>
-        <div className="header-right">
-        {isLoggedIn ? (
-            <button className="header-btn" onClick={goToLibrary}>
-                Library {/* Is Logged in */}
-            </button>
-            ) : (
-              <button className="header-btn" onClick={logIn}>
-                Library {/* Is Logged OUT */}
-              </button>
-            )}
-          {isLoggedIn ? (
-            <button className="header-btn" onClick={logOut}>
-              Log Out
-            </button>
-            ) : (
-              <button className="header-btn" onClick={logIn}>
-                Log In
-              </button>
-            )}
-        </div>
-      </header>
-
-      {/* Sidebar */}
-      <div className={`sidebar ${isSidebarOpen ? 'open' : ''}`}>
-        <button className="filter" onClick={toggleSidebar}>
-          ×
+  return ( 
+    <div className={`app ${isSidebarOpen ? "sidebar-open" : ""}`}>
+    {/* Header Section */}
+    <header>
+      <div className="header-left">
+        <button className="open-btn" onClick={toggleSidebar}>
+          ☰ Filter
         </button>
-        <h2>Filter</h2>
-        <h3>Player Number</h3>
-        <div className="range_container">
-          <div className="sliders_control">
-            <input
-              id="fromSlider"
-              type="range"
-              value={fromValue}
-              min="1"
-              max="8"
-              step="1"
-              aria-label="Minimum player number"
-              onChange={(e) => setFromValue(Math.min(e.target.value, toValue - 1))}
-            />
-            <input
-              id="toSlider"
-              type="range"
-              value={toValue}
-              min="1"
-              max="8"
-              step="1"
-              aria-label="Maximum player number"
-              onChange={(e) => setToValue(Math.max(e.target.value, fromValue + 1))}
-            />
-          </div>
-          <div className="form_control">
-            <span>Min: {fromValue}</span>
-            <span>Max: {toValue}</span>
-          </div>
-        </div>
-        <button className="filter" onClick={() => fetchFilter(fromValue, toValue)}>Filter</button>
       </div>
-
-      {/* Main Content*/}
-      <main>
+      <div className="header-right">
+        {isLoggedIn ? (
+          <button className="header-btn" onClick={goToLibrary}>
+              Library {/* Is Logged in */}
+          </button>
+          ) : (
+          <button className="header-btn" onClick={logIn}>
+            Library {/* Is Logged OUT */}
+          </button>
+        )}
+        {isLoggedIn ? (
+          <button className="header-btn" onClick={logOut}>
+            Log Out {/* Is Logged in */}
+          </button>
+          ) : (
+          <button className="header-btn" onClick={logIn}>
+            Log In  {/* Is Logged OUT */}
+          </button>
+        )}
+      </div>
+    </header>
+    {/* Sidebar */}
+    <div className={`sidebar ${isSidebarOpen ? 'open' : ''}`}>
+      <button className="filter" onClick={toggleSidebar}> × </button>
+      <h2>Filter</h2>
+      <h3>Player Number</h3>
+      <div className="range_container">
+        <div className="sliders_control">
+          <input
+            id="fromSlider"
+            type="range"
+            value={fromValue}
+            min="1"
+            max="8"
+            step="1"
+            aria-label="Minimum player number"
+            onChange={(e) => setFromValue(Math.min(e.target.value, toValue - 1))}/>
+          <input
+            id="toSlider"
+            type="range"
+            value={toValue}
+            min="1"
+            max="8"
+            step="1"
+            aria-label="Maximum player number"
+            onChange={(e) => setToValue(Math.max(e.target.value, fromValue + 1))}
+          />
+        </div>
+        <div className="form_control">
+          <span>Min: {fromValue}</span>
+          <span>Max: {toValue}</span>
+        </div>
+      </div>
+      <button className="filter" onClick={() => fetchFilter(fromValue, toValue)}>Filter</button>
+    </div>
+    {/* Main Content*/}
+    <main>
       <h1>Dashboard Game Database</h1>
       <div className="button-grid" 
-      onScroll={(e) => {
+        onScroll={(e) => {  {/* On user scrolling through box, load more games */}
           const { scrollTop, scrollHeight, clientHeight } = e.target;
           if (scrollTop + clientHeight >= scrollHeight - 10) {loadMoreGames();}}}>
 
         {/* maps each game into its own button */}
-        {games.length === 0 ? (<p>No games available.</p>) : (
-            games.slice(0, visibleGames).map((game) => (
-                <button
-                    key={game.gameId}
-                    className="grid-item"
-                    onClick={() => goToInfo(game.gameId, "true")}
-                    aria-label={`View details for ${game.gameName}`}>
-                    <img
-                        src={game.boxArtUrl} // Ensure property matches your backend
-                        alt={`${game.gameName}`}
-                        className="grid-item-img"/>
-                    <span className="grid-item-text">{game.gameName}</span>
-                </button>
-            ))
-        )}
+        {games.length === 0 ? (<p>No games available.</p>) : 
+        ( games.slice(0, visibleGames).map((game) => (
+          <button
+            key={game.gameId}
+            className="grid-item"   
+            onClick={() => goToInfo(game.gameId, "true")}
+            aria-label={`View details for ${game.gameName}`}>
+              {/* onClick: passes in gameid and location: true = database, false = library */}
+            <img src={game.boxArtUrl} alt={`${game.gameName}`} className="grid-item-img"/>
+            <span className="grid-item-text">{game.gameName}</span>
+          </button>
+        )))}
       </div>
     </main>
-    </div> 
-    
-   );
- };
-  export default Front;
+  </div> 
+  );
+};
+export default Front;
--- src/gameinfo.jsx
+++ src/gameinfo.jsx
@@ -1,212 +1,189 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate, useParams} from 'react-router-dom';
 const GameInfo = () => {
-const { id, loc } = useParams();
-const [isLoggedIn, setIsLoggedIn] = useState(false);
-const [game, setGame] = useState([]);
-const [userId, setuserId] = useState(null);
-const [location, setLocation] = useState();
+  const { id, loc } = useParams(); //passed in parameters. loc) true = database, false = library
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [game, setGame] = useState([]);
+  const [userId, setuserId] = useState(null);
+  const [location, setLocation] = useState();
+  const navigate = useNavigate();
 
- const navigate = useNavigate();
+  const checkLoginStatus = async () => {
+    try {
+      const response = await fetch('http://localhost:3000/test-session', {
+        method: 'GET',
+        credentials: 'include', // include user id on request
+      });
 
- const checkLoginStatus = async () => {
-  try {
-    const response = await fetch('http://localhost:3000/test-session', {
-      method: 'GET',
-      credentials: 'include', // Include cookies in the request
-    });
-
-    if (response.ok) {
-      const data = await response.json();
-      const userId = data.userID; // Extract user ID from response
-      setuserId(userId);
-      setIsLoggedIn(true);
-      console.log('User ID:', userId);
-    } else {
-      const errorData = await response.json();
-      console.error('No active session:', errorData);
+      if (response.ok) {
+        const data = await response.json();
+        const userId = data.userID; // Extract user ID from response
+        setuserId(userId);          //sets user id on this page for add + delete functions
+        setIsLoggedIn(true);        //tells the page youre logged in
+      } else {
+        const errorData = await response.json();
+        console.error('No active session:', errorData);
+      }
+    } catch (error) {
+      console.error('Error fetching user ID:', error);
+      alert('Error fetching user ID.');
     }
-  } catch (error) {
-    console.error('Error fetching user ID:', error);
-    alert('Error fetching user ID.');
-  }
-};
+  };
 
- const logOut = () => {
-  setIsLoggedIn((prevState) => !prevState);
-   navigate('/'); // Navigate to the Login page
- };
+  const logOut = () => {
+  setIsLoggedIn((prevState) => !prevState); //tells the page youre logged out
+    navigate('/'); //nav to the Login page and expires session token(?)
+  };
 
- const logIn = () => {
-  navigate('/'); // Navigate to the Login page
- }
+  const logIn = () => {
+  navigate('/'); //nav to the Login page
+  };
 
- const goToLibrary = () => {
-  navigate('/library');
- }
+  const goToLibrary = () => {
+  navigate('/library'); // nav to library
+  };
 
- const goToFront = () => {
-  navigate('/front');
- }
+  const goToFront = () => {
+  navigate('/front'); //nav to database
+  };
 
- const checkLoc = (where) => {  
-  console.log(where);
-  if(where === "true")
-    setLocation(true);
+  const checkLoc = (where) => {  //called as soon as page loads, loc from parameters is passed in
+  if(where === "true")          //if true was passed in, they came from the database
+    setLocation(true);        
   else
-    setLocation(false);
-  console.log(location);
- }
+    setLocation(false);         //theres no other option so they came from the library
+  };
 
- const navLoc = () => {
+  const navLoc = () => {   //called when user clicks the back button
   if(loc === "true")
-    navigate('/front'); // navigate to front
+    navigate('/front'); // navigate to database
   else
     navigate('/library') //navigate to library
- }
+  };
 
- const fetchGame = async () => {
-  try {
+  const fetchGame = async () => {
+  try {   //calls one specific game that was passed in parameters
     const response = await fetch(`http://localhost:3000/games/${id}`);
+
     if (!response.ok) {
       throw new Error('Failed to fetch game');
     }
-    console.log('game fetched');
     const data = await response.json(); // Fetch the data
     setGame(data); //updates the state
   } catch (error) {
     console.error('Error fetching games:', error);
-  }
-};
+  }};
 
-const addToLibrary = async (status) => {
-  try {
-    console.log("Adding to user library...");
-    console.log(userId, status, id);
-    const response = await fetch(`http://localhost:3000/libraries`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ 
-        userId: userId,
-        gameId: id,
-        status: status,
-      }),
-      credentials: 'include',
-    });
+  const addToLibrary = async (status) => {
+    try {
+      const response = await fetch(`http://localhost:3000/libraries`, {
+        method: 'POST', //posting to user's library
+        headers: {
+          'Content-Type': 'application/json',   //verifies that its a json response
+        },
+        body: JSON.stringify({ 
+          userId: userId,
+          gameId: id,         //data passed into fetch
+          status: status,
+        }),
+        credentials: 'include', //include userId
+      });
+
+      if (!response.ok) {
+        console.log("Library error");
+        return;
+      }
+      console.log("Library Success");
+      alert(`Game added to your library`);  //lets the user know that it was successful
+    } catch(error) {
+      console.error('Error:', error);
+      alert(`Error: ${error.message || 'Unknown Error'}`);
+  }};
+
+  const removeFromLibrary = async (userId) => {
+    try {
+      console.log("Removing from user library...");
+      const response = await fetch(`http://localhost:3000/libraries/${userId}`, {
+        method: 'DELETE',
+      });
+      
+      if (!response.ok) {
+        console.log("Library error");
+        alert("An Error Occurred. Please try again.");
+        return;
+      }
+      console.log("Library Success");
+      alert(`Game removed from your library`);  //lets the user know that it was successful
+    } catch(error) {
+      console.error('Error:', error);
+      alert(`Error: ${error.message || 'Unknown Error'}`);
+  }};
+
+  useEffect(() => {   //runs as soon as the page opens.  order matters
+    checkLoginStatus();
+    checkLoc(loc);
+    fetchGame();
+  }, [userId]);
+
+  const nicerParagraph = (desc) => { //removes weird shit that was parsed into the description probably by accident
+    const remove = ["<br/>", "&quot;", "&amp;", "&mdash;","&egrave;","&ldquo;","&rdquo;"];
     
-    const data = await response.json();
+    let text = typeof desc === "string" ? desc : String(desc); //parses desc into a string
 
-    if (!response.ok) {
-      console.log("Library error");
-      return;
+    if (typeof desc !== "string") {
+      return ""; // Return an empty string if it's not a valid string
     }
-    console.log("Library Success");
-    alert(`Game added to your library`);
-
-  } catch(error) {
-    console.error('Error:', error);
-    alert(`Error: ${error.message || 'Unknown Error'}`);
-  }
-}
-
-const removeFromLibrary = async (userId) => {
-  try {
-    console.log("Removing from user library...");
-
-    const response = await fetch(`http://localhost:3000/libraries/${userId}`, {
-      method: 'DELETE',
-    }); //fix parameter
     
-    const data = await response.json();
+    //removes all of the phrases in the remove array from desc
+    const filteredText = remove.reduce((result, phrase) => result.replaceAll(phrase, " "), text);
+    return filteredText;
+  };
 
-    if (!response.ok) {
-      console.log("Library error");
-      alert("An Error Occurred. Please try again.");
-      return;
-    }
-
-    console.log("Library Success");
-    alert(`Game removed from your library`);
-
-  } catch(error) {
-    console.error('Error:', error);
-    alert(`Error: ${error.message || 'Unknown Error'}`);
-  }
-}
-
-useEffect(() => {
-  checkLoginStatus();
-  checkLoc(loc);
-  fetchGame();
-}, [userId]);
-
-const nicerParagraph = (desc) => {
-  const remove = ["<br/>", "&quot;", "&amp;", "&mdash;","&egrave;","&ldquo;","&rdquo;"];
-  
-  let text = typeof desc === "string" ? desc : String(desc);
-
-  if (typeof desc !== "string") {
-    return ""; // Return an empty string if it's not a valid string
-  }
-  
-  // Filter out phrases
-  const filteredText = remove.reduce(
-    (result, phrase) => result.replaceAll(phrase, " "), 
-    text
-  );
-
-  return filteredText;
-};
-
-   return ( 
-     <div >
+  return ( 
+    <div >
       {/* Header Section */}
       <header>
         <div className="header-left">
           <button className="header-btn" onClick={navLoc}>
-            Back
+            Back  {/* checks location and naviagates you accordingly */}
           </button>
         </div>
         <div className="header-right">
-        {location ? (
-          isLoggedIn ? (
-            <button className="header-btn" onClick={goToLibrary}>
-              Library {/* Is Logged in */}
+          {/* if location is true v */}
+          {location ? (
+            isLoggedIn ? (
+              <button className="header-btn" onClick={goToLibrary}>
+                Library {/* Is Logged in */}
+              </button>
+            ) : (
+              <button className="header-btn" onClick={logIn}>
+                Library {/* Is Logged OUT */}
+              </button>
+            )) : (
+            isLoggedIn ? (
+              <button className="header-btn" onClick={goToFront}>
+                Database {/* Is Logged in */}
+              </button>
+            ) : (
+              <button className="header-btn" onClick={goToFront}>
+                Database {/* Is Logged OUT */}
+              </button>
+          ))}  {/* if location is false ^ */}
+          {isLoggedIn ? (
+            <button className="header-btn" onClick={logOut}>
+              Log Out {/* is logged in */}
             </button>
           ) : (
             <button className="header-btn" onClick={logIn}>
-              Library {/* Is Logged OUT */}
+              Log In  {/* is logged out */}
             </button>
-          )
-        ) : (
-          isLoggedIn ? (
-            <button className="header-btn" onClick={goToFront}>
-              Database {/* Is Logged in */}
-            </button>
-          ) : (
-            <button className="header-btn" onClick={goToFront}>
-              Database {/* Is Logged OUT */}
-            </button>
-          )
-        )}
-
-        {isLoggedIn ? (
-          <button className="header-btn" onClick={logOut}>
-            Log Out
-          </button>
-        ) : (
-          <button className="header-btn" onClick={logIn}>
-            Log In
-          </button>
-        )}
+          )}
         </div>
       </header>
-
       {/* Main Content*/}
       <main>
         <div className="info-container">
+          {/* Left side */}
           <div className="left-info">
             <h1>{game.gameName} {game.releaseDate}</h1>
             <h2>{game.publisher}, {game.leadDesigner}</h2>
@@ -214,26 +191,30 @@ const nicerParagraph = (desc) => {
                 <img src={game.boxArtUrl} alt={game.gameName}/>
             </div>
           </div>
+          {/* Right side */}
           <div className="right-info">
+            {/* everything but desc */}
             <div className="mini-container">
+              {/* if location is true v */}
               {location ? (
-                  isLoggedIn ? (
-                    <button className="filter" onClick={() => addToLibrary("owned")}>
-                      Add to my Library {/* Is Logged in */}
-                    </button>
-                  ) : (
-                    <button className="filter" onClick={logIn}>
-                      Add to my Library {/* Is Logged OUT */}
-                    </button>
-                  )) : (
-                    <button className="filter" onClick={() => removeFromLibrary("owned")}>
-                      Remove from my Library {/* Is Logged in */}
-                    </button>
-                )}
+                isLoggedIn ? (
+                  <button className="filter" onClick={() => addToLibrary("owned")}>
+                    Add to my Library {/* Is Logged in */}
+                  </button>
+                ) : (
+                  <button className="filter" onClick={logIn}>
+                    Add to my Library {/* Is Logged OUT */}
+                  </button>
+                )) : (
+                  <button className="filter" onClick={() => removeFromLibrary("owned")}>
+                    Remove from my Library {/* Is Logged in */}
+                  </button>)}  
+              {/* if location is false ^ */}
               <h2>Players: {game.minPlayers} - {game.maxPlayers}</h2>
               <h2>Age Suggestion: {game.age}+</h2>
               <h2>Avg. Playtime: {game.playTime} min</h2>
             </div>
+            {/* desc */}
             <div className="paragraph-handler">
               <p>{nicerParagraph(game.description)}</p>
             </div>
@@ -241,7 +222,6 @@ const nicerParagraph = (desc) => {
         </div>
       </main>
     </div>
-    
-   );
- };
-  export default GameInfo;
+  );
+};
+export default GameInfo;
